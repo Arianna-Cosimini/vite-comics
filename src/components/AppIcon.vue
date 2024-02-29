f037884ec6a22b52fd54134161c1f970084c05a7
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-29 15:59:16 +0100

    creato in data array di oggetti per stampare in pagina le icone con il testo

--- src/components/AppIcon.vue
+++ src/components/AppIcon.vue
@@ -1,6 +1,33 @@
 <script>
 export default {
     name: "AppIcon",
+
+    data(){
+        return{
+            icons:[
+                {
+                    img:'../../public/img/buy-comics-digital-comics.png',
+                    text:'digital comics'
+                },
+                {
+                    img:'../../public/img/buy-comics-merchandise.png',
+                    text:'dc merchandise'
+                },
+                {
+                    img:'../../public/img/buy-comics-subscriptions.png',
+                    text:'subscription'
+                },
+                {
+                    img:'../../public/img/buy-comics-shop-locator.png',
+                    text:'comic shop locator'
+                },
+                {
+                    img:'../../public/img/buy-dc-power-visa.svg',
+                    text:'dc power visa'
+                },
+            ]
+        }
+    }
 }
 </script>
 
