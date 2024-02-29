5f2436d5bc1c3f60750bef5592f59b4bd8d6206a
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-29 15:02:32 +0100

    fine stilizzazione statica della navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -14,16 +14,56 @@ export default {
 
             <div class="menu-links">
                 <ul>
-                    <li>characters</li>
-                    <li>comics</li>
-                    <li>movies</li>
-                    <li>tv</li>
-                    <li>games</li>
-                    <li>collectibles</li>
-                    <li>videos</li>
-                    <li>fans</li>
-                    <li>news</li>
-                    <li>shop</li>
+                    <li>
+                        <a href="#">
+                            characters
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            movies
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            tv
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            games
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            collectibles
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            videos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            fans
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            news
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            shop
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -32,15 +72,46 @@ export default {
 </template>
 
 <style lang="scss">
+@use '../styles/variables' as *;
+@use '../styles/mixins' as *;
 
-#menu{
-    max-width: 1200px;
-    margin: 0 auto;
+#menu {
+    @include styleContainer;
 
     display: flex;
     justify-content: space-between;
+    align-items: center;
 }
 
+.menu-links {
+
+    ul {
+        display: flex;
+        gap: 40px;
 
 
+        list-style-type: none;
+        font-size: 12px;
+        font-weight: 700;
+
+
+        li {
+            text-transform: uppercase;
+
+
+            a {
+                text-decoration: none;
+                color: black;
+                padding: 20px 0;
+
+
+                &:hover {
+                    color: $PrimaryColor;
+                    border-bottom: 5px solid  $PrimaryColor;
+                }
+
+            }
+        }
+    }
+}
 </style>
