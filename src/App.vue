8b64beb0bf408168a457fbe799469c7218399777
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-01 14:55:25 +0100

    creato layout statico

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,10 @@ export default {
   <AppFooter></AppFooter>
 </template>
 
-<style></style>
+<style>
+
+body{
+  margin: 0;
+}
+
+</style>
---
+++ src/components/CardItems.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    name: 'CardItems',
+
+   
+    props: {
+        // dobbiamo dare un nome a queste proprietà
+        // per ognuna di esse dobbiamo indicare nome e TIPO DI DATO
+        // String, Number, Boolean, Array, Object
+        // il tipo di dato deve iniziare con la lettera maiuscola
+        cardName: String,
+        cardImage: String
+    },
+
+    data() {
+    },
+}
+</script>
+
+<template>
+
+    <div class="cards">
+        <img :src="cardImage" :alt="cardName">
+        <h4>{{ cardName }}</h4>
+    </div>
+
+</template>
+
+<style lang="scss">
+
+// .cards{
+//     display: flex;
+//     flex-direction: column;
+//     width: calc(100% / 6 - 20px);
+//     padding: 20px 0;
+
+//     img{
+//         width: 100%;
+//     }
+// }
+
+</style>
